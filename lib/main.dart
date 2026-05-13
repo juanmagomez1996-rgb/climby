6b28e5917437fb8d842f1c41932719ad81809618
juanmagomez1996-rgb <juanmagomez1996@gmail.com>
2026-05-12 20:45:29 -0500

    Load game assets in main function
    Initialize game assets during app startup.

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'climby_app.dart';
 import 'storage.dart';
+import 'render.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Prefs.init();
+  await GameAssets.loadAll();
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
   ]);
